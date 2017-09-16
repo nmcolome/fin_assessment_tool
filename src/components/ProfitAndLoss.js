a8faf5a1e56d22efacb28a6aecbf2306d9180bcc
NataliaColome <nmcolome@gmail.com>
2017-09-15 21:16:59 -0600

    Build TableColumn component and css; Add it to Profit&Loss components

--- src/components/ProfitAndLoss.js
+++ src/components/ProfitAndLoss.js
@@ -31,7 +31,24 @@ class ProfitAndLoss extends Component {
     })
     return (
       <div className='profit'>
+        <section className='data-table'>
+        <section className='data-column'>
+          <articles className='data-header'>Month</articles>
+          <articles className='data-header'>Gross Sales</articles>
+          <articles className='data-header'>Discounts</articles>
+          <articles className='data-header'>Net Sales</articles>
+          <articles className='data-header'>COGS</articles>
+          <articles className='data-header'>Gross Profit</articles>
+          <articles className='data-header'>Operating Expense</articles>
+          <articles className='data-header'>Net Profit</articles>
+        </section>
+          <section className='data-list'>
+            {column_data}
+          </section>
+        </section>
+        <section>
 
+        </section>
       </div>
     )
   }
---
+++ src/components/TableColumn.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles/TableColumn.css'
+import moment from 'moment'
+
+const TableColumn = ({data}) => {
+  return(
+    <section className='data-column'>
+      <articles className='data-cel'>{moment(data.date).format("MMM YY")}</articles>
+      <articles className='data-cel'>{(data.sales/1000).toFixed(2)}</articles>
+      <articles className='data-cel'>{(data.discount/1000).toFixed(2)}</articles>
+      <articles className='data-cel'>{(data.net_sales/1000).toFixed(2)}</articles>
+      <articles className='data-cel'>{(data.cogs/1000).toFixed(2)}</articles>
+      <articles className='data-cel'>{(data.gross_profit/1000).toFixed(2)}</articles>
+      <articles className='data-cel'>{(data.op_expense/1000).toFixed(2)}</articles>
+      <articles className='data-cel'>{(data.op_income/1000).toFixed(2)}</articles>
+    </section>
+  )
+}
+
+export default TableColumn
